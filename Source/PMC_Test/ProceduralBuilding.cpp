0837ad06874efb4469c3ea0945b47be051cd5b88
Andrew <a.ermak@innopolis.ru>
2018-02-18 01:39:24 +0300

    1

--- Source/PMC_Test/ProceduralBuilding.cpp
+++ Source/PMC_Test/ProceduralBuilding.cpp
@@ -35,20 +35,19 @@ void AProceduralBuilding::ConstructBuild()
     float width = (Width * BlockWidth);
     wall_1 = ConstructWall(FVector(0, - BlockWidth + BlockDepth / 2, 0), FRotator(0, 0, 0));
     wall_2 = ConstructWall(FVector(0, - BlockDepth / 2, 0), FRotator(0, 90, 0));
-    wall_3 = ConstructWall(FVector(- width + BlockWidth,
-                                   - width + BlockDepth / 2, 0), FRotator(0, 180, 0));
-    wall_4 = ConstructWall(FVector(- width + BlockWidth + 0,
-                                     width - BlockWidth - BlockDepth / 2, 0), FRotator(0, 270, 0));
-//    this->wall_1->setOffsetOdd(true);
-//    this->wall_4->setOffsetOdd(true);
+    wall_3 = ConstructWall(FVector(- width + BlockWidth - BlockDepth,
+                                   - width - BlockDepth / 2, 0), FRotator(0, 180, 0));
+    wall_4 = ConstructWall(FVector(- width + BlockWidth - BlockDepth,
+                                     width - BlockWidth / 2, 0), FRotator(0, 270, 0));
+    this->wall_1->setOffsetOdd(true);
+    this->wall_3->setOffsetOdd(true);
 }
 
 AProceduralWallTest* AProceduralBuilding::ConstructWall(FVector location, FRotator rotation)
 {
     AProceduralWallTest* wall = GetWorld()->SpawnActor<AProceduralWallTest>(AProceduralWallTest::StaticClass()) ;
     
-    wall->setOffset(false);
-//    wall->SetActorLocation(location);
+    wall->setOffset(true);
     wall->SetValues(Height, Width, StaticMesh, Material);
     wall->Initialize();
     wall->spawnObject(location);
